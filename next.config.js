94c6763f9d2a1713afdd6550cfd505558b8374e2
Josh Worley <helloworley@gmail.com>
2020-10-18 21:39:00 -0500

    downgraded webpack to 4.42.0

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
-
 module.exports = withCSS(withSass({
     target: 'serverless',
-    env: {
-        CONTENTFUL_SPACE: 'tgk6k1pkyqnt',
-        CONTENTFUL_TOKEN: 'HcyrB461jAGcWAHXpGjp8P_P4f8t4dGsvqkbJ9Aw0eo'
-    },
-    webpack (config) {
-        config.node = {
-            fs: "empty"
-        };
-        config.plugins = [
-            ...config.plugins,
-        ]
+    webpack (config, options) {
        config.module.rules.push({
            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
            use: {
@@ -21,7 +10,7 @@ module.exports = withCSS(withSass({
                options: {
                    limit: 100000
                }
-           },
+           }
        });
 
        return config;
