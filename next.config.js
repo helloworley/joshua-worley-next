fea32121236622f1c85d73fd6f6108770bf99ff6
Josh Worley <helloworley@gmail.com>
2020-10-18 20:55:07 -0500

    updated contentful space to next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(withSass({
     target: 'serverless',
+    env: {
+        CONTENTFUL_SPACE: 'tgk6k1pkyqnt',
+        CONTENTFUL_TOKEN: 'HcyrB461jAGcWAHXpGjp8P_P4f8t4dGsvqkbJ9Aw0eo'
+    },
     webpack (config, options) {
        config.module.rules.push({
            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
