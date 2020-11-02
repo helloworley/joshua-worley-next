19dbfb8d56f86e72a67882eefc2139908a2b5ea2
Josh Worley <helloworley@gmail.com>
2020-11-02 15:34:03 -0600

    hardcoded next SEO default contnent

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Head from 'next/head';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import themeStyles from '../style/theme'
+import { DefaultSeo } from 'next-seo'
 
 
 
@@ -28,6 +29,26 @@ class MyApp extends App {
         <link rel="shortcut icon" href="/static/favicon.ico" />
         {/* <title>Joshua Worley</title> */}
       </Head>
+        <DefaultSeo
+           title={contentfulData.seoTitle}
+           description={contentfulData.seoDescription}
+           openGraph={{
+             type: 'website',
+             locale: 'en_IE',
+             url: 'https://www.joshuaworley.com/',
+             site_name: 'Joshua Worley Portfolio',
+             title: 'Joshua Worley',
+             description: 'Digital Designer (App, Web, UI, UX, Product) & Frontend Developer. Joshua Worley solves complicated problems with simple solutions.',
+             images: [
+               {
+                 url: '//images.ctfassets.net/tgk6k1pkyqnt/4xRoBfh7GCKAct5adLGng7/48d351d31ebc016ffa12129e087c2c99/jw-sns-og.png',
+                 width: 800,
+                 height: 600,
+                 alt: 'Joshua Worley Portfolio Site',
+               }
+             ],
+           }}
+        />
         <CssBaseline />
         <MuiThemeProvider theme={theme}>
           <AnimatePresence exitBeforeEnter>
