5842f7bf98b67b2b632b9d50e5caf21294ec74f5
Josh Worley <helloworley@gmail.com>
2021-01-04 17:26:18 -0600

    took out featured project section, updated card styles

--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -7,11 +7,8 @@ import { Grid } from '@material-ui/core/'
 
 const useStyles = makeStyles(theme => ({
   projects: {
-    maxWidth: '1560px',
-    margin: '0 auto',
     padding: theme.padding.xs,
-    borderTop: `solid 1px ${theme.colors.line}`,
-    // backgroundColor: theme.colors.sand,
+    backgroundColor: theme.colors.gray1,
     [theme.breakpoints.up('sm')]: {
       padding: '120px 20px'
     },
@@ -21,6 +18,10 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('lg')]: {
       padding: '120px 30px'
     },
+  },
+  projectsInner: {
+    maxWidth: '1560px',
+    margin: '0 auto',
   },
   infoWrapper: {
     margin: '0 auto',
@@ -32,15 +33,6 @@ const useStyles = makeStyles(theme => ({
   image: {
     width: '100%'
   },
-  // gridItem: {
-  //   paddingRight: '40px',
-  //   '&:nth-of-type(1)': {
-  //     paddingLeft: '10px'
-  //   },
-  //   '&:nth-of-type(4)': {
-  //     paddingRight: '10px',
-  //   }
-  // }
 }));
 
 const Projects = props => {
@@ -48,41 +40,45 @@ const Projects = props => {
 
   return (
     <div className={classes.projects}>
-      <SectionHeading text={props.title} noBorder={true} />
-      <Grid container spacing={6}>
-        {
-          props.projects.map( service => {
-            // console.log('service', service)
-            const brand = service.brand["en-US"]
-            const slug = service.slug["en-US"]
-            const brandAbout = service.brandAbout["en-US"].content
-            const date = service.date["en-US"]
-            const finalThoughts = service.finalThoughts["en-US"].content
-            const heroImage = service.heroImage["en-US"]
-            const logo = service.logo["en-US"]
-            const projectIntro = service.projectIntro["en-US"].content
-            const projectTitle = service.projectTitle["en-US"]
-            const projectType = service.projectType["en-US"]
-            const sections = service.sections["en-US"]
-            return <Grid item xs={12} sm={6} lg={3} key={brand} className={classes.gridItem}>
-              <ImageCard 
-                urlBase="/projects"
-                slug={slug}
-                brandAbout={brandAbout}
-                date={date}
-                finalThoughts={finalThoughts}
-                image={heroImage}
-                logo={logo}
-                projectIntro={projectIntro}
-                projectTitle={projectTitle}
-                projectType={projectType}
-                sections={sections}
-              />
-            </Grid>
-          })
-        }
-        
-      </Grid>
+      <div className={classes.projectsInner}>
+        <SectionHeading text={props.title} noBorder={true} />
+        <Grid container spacing={6}>
+          {
+            props.projects.map( service => {
+              console.log('service (project)', service)
+              const brand = service.brand["en-US"]
+              const slug = service.slug["en-US"]
+              const brandAbout = service.brandAbout["en-US"].content
+              const date = service.date["en-US"]
+              const finalThoughts = service.finalThoughts["en-US"].content
+              const heroImage = service.heroImage["en-US"]
+              const logo = service.logo["en-US"]
+              const projectIntro = service.projectIntro["en-US"].content
+              const projectTitle = service.projectTitle["en-US"]
+              const projectType = service.projectType["en-US"]
+              const sections = service.sections["en-US"]
+              const tech = service.technologies["en-US"]
+              return <Grid item xs={12} sm={6} lg={3} key={brand} className={classes.gridItem}>
+                <ImageCard 
+                  urlBase="/projects"
+                  slug={slug}
+                  brandAbout={brandAbout}
+                  date={date}
+                  finalThoughts={finalThoughts}
+                  image={heroImage}
+                  logo={logo}
+                  projectIntro={projectIntro}
+                  projectTitle={projectTitle}
+                  projectType={projectType}
+                  sections={sections}
+                  tech={tech}
+                />
+              </Grid>
+            })
+          }
+          
+        </Grid>
+      </div>
     </div>
   )
 };
