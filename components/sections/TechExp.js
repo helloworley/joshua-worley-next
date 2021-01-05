b85defbb801093716659c91e43bd74bed9b62e11
Josh Worley <helloworley@gmail.com>
2021-01-04 21:25:49 -0600

    fixed slight whitespace problem under techs

--- components/sections/TechExp.js
+++ components/sections/TechExp.js
@@ -8,7 +8,7 @@ const useStyles = makeStyles(theme => ({
     margin: '0 auto'
   },
   techExp: {
-    padding: '100px 10px'
+    padding: '100px 10px 0'
   },
   displayTechs: {
     display: 'grid',
