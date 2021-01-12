bb1e4c60280245af99ca41664b54b958f8cf09d9
Josh Worley <helloworley@gmail.com>
2021-01-12 12:31:00 -0600

    added a background to the thank you component

--- components/sections/ThankYou.js
+++ components/sections/ThankYou.js
@@ -1,26 +1,30 @@
 import { makeStyles } from '@material-ui/core/styles';
-import RichTextToHTML from '../RichTextToHTML'
-import SectionHeading from '../layout/SectionHeading'
-
-
+import RichTextToHTML from '../RichTextToHTML';
+import SectionHeading from '../layout/SectionHeading';
+const bgImage = require('../../public/sanfran.jpg');
 
 const useStyles = makeStyles(theme => ({
   thankYou: {
-    padding: theme.padding.xs,
-    backgroundColor: theme.colors.white,
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
     '& p': {
       color: theme.colors.gray700,
     },
     '& h2': {
       color: theme.colors.gray700,
     },
+  },
+  thankYouInner: {
+    backgroundColor: 'rgba(255,255,255,.85)',
+    padding: theme.padding.xs,
     [theme.breakpoints.up('sm')]: {
       padding: theme.padding.sm
     },
     [theme.breakpoints.up('md')]: {
       padding: '90px 0 110px'
     },
-  },
+  }
 }));
 
 const ThankYou = props => {
@@ -30,10 +34,12 @@ const ThankYou = props => {
 
   return (
     <div className={classes.thankYou}>
-      <div className="section-wrapper">
-        <SectionHeading text="Thank You" />
-        <div className="thank-you-section">
-          <RichTextToHTML data={thankYouText} />
+      <div className={classes.thankYouInner}>
+        <div className="section-wrapper">
+          <SectionHeading text="Thank You" />
+          <div className="thank-you-section">
+            <RichTextToHTML data={thankYouText} />
+          </div>
         </div>
       </div>
     </div>
